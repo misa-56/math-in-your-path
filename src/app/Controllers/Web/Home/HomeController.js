cc78fb93a3eb04b228eaeda90b7e074b6a51a427
Misha <hoangt@thebigrichgroup.com>
2023-05-17 17:33:34 +0700

    update User's Home Page

--- src/app/Controllers/Web/Home/HomeController.js
+++ src/app/Controllers/Web/Home/HomeController.js
@@ -4,19 +4,19 @@ class HomeController {
 
     async index(req, res) 
     {
-        const limit = 2; // Number of articles per page
-        const offset = parseInt(req.query.offset) || 0; // Start offset
+        // const limit = 2; // Number of articles per page
+        // const offset = req.query.page ? (req.query.page - 1) * limit : 0;
         const articles = await Article.findAll({
-            limit,
+            raw: true,
+            limit: 20,
             offset,
             order:[['createdAt', 'DESC']],
         });
-        console.log('articles', articles);
 
         const featuredArticles = await Article.findAll({
             raw: true,
             order:[['createdAt', 'DESC']],
-            limit: 3,
+            limit: 4,
         });
         const modifiedFeaturedArticles = featuredArticles.map(article => {
             return {
@@ -25,24 +25,19 @@ class HomeController {
               // Add more custom fields as needed
             };
         });
-        // featuredArticles.createdAt = featuredArticles.createdAt.toISOString().split('T')[0];
-        console.log(modifiedFeaturedArticles[0]);
-
-        // const articlesWithOwnProperties = articles.map(article => Object.assign({}, article.get({ plain: true })));
 
         const articlesWithOwnProperties = articles.map(article => {
-            const plainArticle = article.get({ plain: true });
-            // Convert the updatedAt property to an ISO date format
-            plainArticle.slug = plainArticle.title.replace(/ /g, '-');
-            // console.log('slug', plainArticle.slug);
-            plainArticle.updatedAt = plainArticle.updatedAt.toISOString().split('T')[0];
-            return plainArticle;
+            article.slug = article.title.replace(/ /g, '-');
+            article.updatedAt = article.updatedAt.toISOString().split('T')[0];
+            return article;
           });
 
-          res.render('partials/user/main/home/home', 
-          { articles: articlesWithOwnProperties, 
-            featured: modifiedFeaturedArticles.slice(1, 3), 
-            newestFeatured: modifiedFeaturedArticles[0] });
+        res.render('partials/user/main/home/home',
+        {
+            articles: articlesWithOwnProperties, 
+            featured: modifiedFeaturedArticles.slice(1, 4), 
+            newestFeatured: modifiedFeaturedArticles[0] 
+        });
     }
 }
 
