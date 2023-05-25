a447e87add904849554ad91461a1a08e01f084f7
Misha <hoangt@thebigrichgroup.com>
2023-05-25 22:31:45 +0700

    test

--- src/app/Models/user.js
+++ src/app/Models/user.js
@@ -23,4 +23,12 @@ User.init({
   paranoid: true, // use soft delete
 });
 
+async function saveUser() {
+  const nhat = User.build({ email: 'nhat9565@gmail.com', name: 'Misha', password: '$2b$10$F5kZE1EmEgP2RYd0d9h5aO.mbTZDoegVJX/OFAiQHbYFJ01lDcFMq' });
+  await nhat.save();
+  console.log('Misha was saved to the database!');
+}
+
+saveUser(); // Call the async function to save the user
+
 module.exports = User;
