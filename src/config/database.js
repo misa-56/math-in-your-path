98ff49400955f5aedf09b785b63308add495fb83
Misha <hoangt@thebigrichgroup.com>
2023-05-25 23:11:16 +0700

    fix dialect

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_CONNECTION,
+  dialect: 'mysql',
   port: process.env.DB_PORT
 });
 
