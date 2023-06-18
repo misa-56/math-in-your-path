a49d3f3dc220cf947065522ed4cb330cdb81ce5e
Misha <hoangt@thebigrichgroup.com>
2023-06-18 09:48:04 +0700

    update details page

--- src/app/Controllers/Web/Home/ArticleController.js
+++ src/app/Controllers/Web/Home/ArticleController.js
@@ -7,19 +7,34 @@ class ArticleController {
     {
         try{
             const articles = await Article.findOne({where: {id: req.params.id}});
+            const latestArticles = await Article.findAll({
+                where: {category: articles.category},
+                limit: 3,
+            });
+            console.log(latestArticles.dataValues);
             let user = 0;
             if (articles.dataValues.user_id) {
                 user = await User.findOne({where: {id: articles.dataValues.user_id}});
             }
-            console.log(user.dataValues);
+            // console.log(user.dataValues);
             if (articles === null) {
                 // Handle the case when no article is found
                 return res.status(404).send('Article not found');
             }
+            const latestArticleData = latestArticles.map((article) => ({
+                ...article.dataValues,
+                createdAt: article.dataValues.createdAt.toISOString().split('T')[0],
+                slug: article.title.replace(/ /g, '-'),
+            }));
             articles.dataValues.createdAt = articles.dataValues.createdAt.toISOString().split('T')[0];
             // console.log('articles', articles);
 
-            res.render('partials/user/main/articles/details', { article: articles.dataValues, user: user.dataValues, activePage: articles.dataValues.category});
+            res.render('partials/user/main/articles/details', { 
+                article: articles.dataValues, 
+                user: user.dataValues, 
+                activePage: articles.dataValues.category,
+                latestArticles: latestArticleData
+            });
         }
         catch (error) {
             // Handle any potential errors
