490e8e222cf0448022be28f9ad97091078a594cd
Misha <hoangt@thebigrichgroup.com>
2023-06-25 12:22:47 +0700

    write script update article

--- src/app/Controllers/Web/Admin/ArticleController.js
+++ src/app/Controllers/Web/Admin/ArticleController.js
@@ -31,6 +31,51 @@ class ArticleController {
           user_id: req.session.user.id
         });
 
+        res.redirect('/kingslanding');
+
+      }
+      else{
+        res.send('Not added to db');
+      }
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+
+  async edit (req, res)
+  {
+    let categories;
+    try {
+      const articles = await Article.findOne({where: {id: req.params.id}});
+      await Article.findAll({group: ['category'], 
+      attributes: ['category'],
+    })
+      .then((results) => {
+        categories = results.map((result) => result.category);
+        console.log('categories', categories);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(categories);
+      if (articles === null) {
+        // Handle the case when no article is found
+        return res.status(404).send('Article not found');
+      }
+      articles.dataValues.createdAt = articles.dataValues.createdAt.toISOString().split('T')[0];
+
+      res.render('partials/admin/main/articles/edit', {layout: 'admin.hbs', article: articles.dataValues, categories });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async update (req, res)
+  {
+    try {
+      const article = await Article.findByPk(req.params.id);
+      await article.update(req.body);
       await Article.update({content: `<p>In this article, we will explore how to build a Bitcoin Tracker application using Socket.IO and Express.js while following the Model-View-Controller (MVC) architectural pattern. By leveraging the power of real-time communication with Socket.IO and the flexibility of Express.js, we can create a dynamic application that tracks Bitcoin prices and updates them in real-time.</p>
       <div class="m-5">
       <p>Current Bitcoin Price: <span id="price"></span></p>
@@ -81,52 +126,6 @@ class ArticleController {
           });
       </script>`},
       {where: {id: 16}});
-
-        res.redirect('/kingslanding');
-
-      }
-      else{
-        res.send('Not added to db');
-      }
-    }
-    catch(error){
-      console.error(error);
-    }
-  }
-
-  async edit (req, res)
-  {
-    let categories;
-    try {
-      const articles = await Article.findOne({where: {id: req.params.id}});
-      await Article.findAll({group: ['category'], 
-      attributes: ['category'],
-    })
-      .then((results) => {
-        categories = results.map((result) => result.category);
-        console.log('categories', categories);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    console.log(categories);
-      if (articles === null) {
-        // Handle the case when no article is found
-        return res.status(404).send('Article not found');
-      }
-      articles.dataValues.createdAt = articles.dataValues.createdAt.toISOString().split('T')[0];
-
-      res.render('partials/admin/main/articles/edit', {layout: 'admin.hbs', article: articles.dataValues, categories });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async update (req, res)
-  {
-    try {
-      const article = await Article.findByPk(req.params.id);
-      await article.update(req.body);
       res.redirect('/kingslanding');
     } catch (error) {
       console.error(error);
